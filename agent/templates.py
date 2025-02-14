c57c5b9f0f985ed378a107900fa5afe90b8e4759
Alex Potashnik <alex.potashnik@gmail.com>
2025-02-13 18:45:53 -0800

    minor

--- agent/templates.py
+++ agent/templates.py
@@ -4,7 +4,7 @@ from enum import StrEnum
 class PromptTemplate(StrEnum):
     REACHOUT = ('Write an email to a prospective client for a real estate agent. '
                 'Agent\'s name is {agent_name}. Client\'s name is {client_name}. '
-                'Emphasize long track record of success getting home buyers into the home of their dreams.'
+                'Emphasize long track record of success getting home buyers into the home of their dreams. '
                 'Suggest a friendly coffee meeting to discuss I can help.')
 
     FOLLOWUP = ('Write a follow up email from a real estate agent to a client. '
@@ -12,7 +12,7 @@ class PromptTemplate(StrEnum):
                 'Last email was written {timeout} ago and the text was {text}')
 
     RESPOND_TO_COUNTERPARTY = ('Write an email from a real estate agent to a client interested in buying a particular '
-                               'property after receiving communication from a seller.'
+                               'property after receiving communication from a seller. '
                                'Agent\'s name is {agent_name}. Client\'s name is {client_name}. '
                                'Seller\'s name is {counterparty} who wrote {text}')
 
